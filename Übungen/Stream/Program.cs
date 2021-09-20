0868d6d4624fcc4438975dba087c2c0ae73c377c
Liam Walsh <liam.walsh@roche.com>
2021-09-20 10:03:21 +0200

    Create Übungen Project

---
+++ Übungen/Stream/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
