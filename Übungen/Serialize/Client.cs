077f6965a25979432ed13f0851ad6b05d24faebb
Liam Walsh <liam.walsh@roche.com>
2021-09-27 09:15:04 +0200

    Finish new Project

---
+++ Übungen/Serialize/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialize
+{
+    [Serializable]
+    public class Client
+    {
+        private int clientNumber;
+        private string firstname;
+        private string surName;
+        private double factor;
+        [NonSerialized]
+        int regionNumber;
+
+        public Client(int pClientNumber, string pFirstName, string pSurName, double pFactor,
+                        int pRegionNumber)
+        {
+            clientNumber = pClientNumber;
+            firstname = pFirstName;
+            surName = pSurName;
+            factor = pFactor;
+            regionNumber = pRegionNumber;
+        }
+
+        public void printClient()
+        {
+            Console.WriteLine($"Client number: {clientNumber}");
+            Console.WriteLine($"First Name: {firstname}");
+            Console.WriteLine($"Surname: {surName}");
+            Console.WriteLine($"Factor: {factor}");
+            Console.WriteLine($"Region Number: {regionNumber}");
+        }
+    }
+}
--- Übungen/Serialize/Program.cs
+++ Übungen/Serialize/Program.cs
@@ -1,12 +1,118 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Serialize
 {
     class Program
     {
+        static string filename = @"C:\Users\walshl11\Documents\M226B\clientfile.bin";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region First Example
+            //Client[] clients = new Client[2];
+            //clients[0] = new Client(1, "Joe", "Hanson", 1.234, 6300);
+            //clients[1] = new Client(2, "Tina", "Turner", 4.5, 8300);
+
+            //Console.WriteLine("Saved Client;\n");
+            //foreach(Client count in clients)
+            //{
+            //    count.printClient();
+            //    Console.WriteLine();
+            //}
+
+            //FileStream fs = new FileStream(filename, FileMode.Create);
+            //IFormatter bf = new BinaryFormatter();
+
+            //// Writes an Array of objects to a binary file
+            //bf.Serialize(fs, clients);
+
+            //fs.Position = 0;
+            //Console.WriteLine("\n\nReconstructed Clients:\n");
+
+            //// Reads an array of object from a binary file
+            //Client[] recClients = (Client[])bf.Deserialize(fs);
+
+            //foreach(Client count in recClients)
+            //{
+            //    count.printClient();
+            //    Console.WriteLine();
+            //}
+            //fs.Close();
+            #endregion
+
+            #region Second Example
+
+            //List<Client> list1 = new List<Client>(); 
+            //list1.Add(new Client(1, "Joe", "Hanson", 1.234, 6300)); 
+            //list1.Add(new Client(2, "Tina", "Turner", 4.5, 8300));
+
+            //Console.WriteLine("Saved Clients;\n");
+            //foreach (Client count in list1)
+            //{
+            //    count.printClient();
+            //    Console.WriteLine();
+            //}
+
+            //FileStream fs = new FileStream(filename, FileMode.Create); 
+            //IFormatter bf = new BinaryFormatter();   
+
+            //// Writes a list of objects to a binary file
+            //bf.Serialize(fs, list1);   
+
+            //fs.Position = 0; 
+            //Console.WriteLine("\n\nReconstructed Clients:\n"); 
+
+            //// Reads a list of objects from a binary file
+            //List<Client> recList = (List<Client>)bf.Deserialize(fs);
+
+            //foreach (Client count in recList)       
+            //{         
+            //    count.printClient(); 
+            //    Console.WriteLine();       
+            //}       
+
+            //fs.Close(); 
+
+
+            #endregion
+
+            #region Third Example
+
+            ObservableCollection<Client> oclist1 = new ObservableCollection<Client>(); 
+            oclist1.Add(new Client(1, "Joe", "Hanson", 1.234, 6300)); 
+            oclist1.Add(new Client(2, "Tina", "Turner", 4.5, 8300)); 
+            Console.WriteLine("Safed Client;\n"); 
+            foreach (Client count in oclist1) 
+            { 
+                count.printClient(); 
+                Console.WriteLine(); 
+            }
+
+            FileStream fs = new FileStream(filename, FileMode.Create); 
+            IFormatter bf = new BinaryFormatter();
+
+            // Writes anObservableCollection of objects to a binary file
+            bf.Serialize(fs, oclist1);
+            
+            fs.Position = 0; 
+            Console.WriteLine("\n\nReconstructed Clients:\n"); 
+            
+            // Reads an ObservableCollection of objects from a binary file
+            ObservableCollection<Client> recOcList =         
+                (ObservableCollection<Client>)bf.Deserialize(fs); 
+            
+            foreach (Client count in recOcList)       
+            {         
+                count.printClient(); 
+                Console.WriteLine();       
+            }       
+            fs.Close(); 
+
+            #endregion
         }
     }
 }
