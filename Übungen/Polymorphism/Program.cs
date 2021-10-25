89c3f6a4bab0c972a65d92149a3fdd0cbcf27d97
Liam Walsh <liam.walsh@roche.com>
2021-10-25 08:39:24 +0200

    Minor changers

--- Übungen/Polymorphism/Program.cs
+++ Übungen/Polymorphism/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Polymorphism.Classes;
+using Polymorphism.Interfaces;
+using System;
+using System.Linq;
 
 namespace Polymorphism
 {
@@ -6,7 +9,25 @@ namespace Polymorphism
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = new Person();
+            Customer customer = new Customer();
+            SalesManager salesmanager = new SalesManager();
+            person.Children.ToList().Add(salesmanager);
+            person.Parent = salesmanager;
+
+
+            IManageableEntity location = new Person();
+            IManageableEntity lol = new SalesManager();
+
+            Console.ReadKey();
+
+            Location location1 = new Location();
+            ILocation location2 = new Location();
+
+
+            Person person1 = new Person();
+            Person person45 = new Customer();
+            Customer person3 = new Customer();
         }
     }
 }
